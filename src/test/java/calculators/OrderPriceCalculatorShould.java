c6bf28895e425442d204dfef77dccd74de992233
JoshiRaez <joseraezrodriguez@gmail.com>
2020-01-11 09:34:15 +0100

    Finished migrating the test to the calculator own test class

--- src/test/java/calculators/OrderPriceCalculatorShould.java
+++ src/test/java/calculators/OrderPriceCalculatorShould.java
@@ -1,21 +1,17 @@
 package calculators;
 
-import main.FileCalculator;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.function.BiFunction;
-import java.util.function.Function;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.contentOf;
 
 class OrderPriceCalculatorShould {
 
-    private final static String CUSTOMERS_CSV = "customers.csv";
     private final static String ORDERS_CSV = "orders.csv";
     private final static String PRODUCTS_CSV = "products.csv";
     private final static String RESULT_CSV = "result.csv";
@@ -44,10 +40,10 @@ class OrderPriceCalculatorShould {
     void whenThereAreOneOrderWithOneItemGetThePriceOfThatItem() throws IOException {
         //Given
         final String testName = "whenThereAreOneOrderWithOneItemGetThePriceOfThatItem";
-        final FileCalculator fileCalculator = buildFileCalculatorForTestWithOrderPrice(testName, TASK);
-        final File expected = withTestName.apply(testName, getResult).apply(TASK);
+        final OrderPriceCalculator orderPriceCalculator = buildCalculator(testName);
+        final File expected = getExpected(testName);
         //When
-        final File result = fileCalculator.calculateOrderPrices();
+        final File result = orderPriceCalculator.calculateOrderPrices();
         //Then
         assertThat(result).exists();
         assertThat(contentOf(result))
@@ -60,10 +56,10 @@ class OrderPriceCalculatorShould {
     void whenThereIsOneOrderWithMultipleProductsThePriceShouldBeTheSumOfPrices() throws IOException {
         //Given
         final String testName = "whenThereIsOneOrderWithMultipleProductsThePriceShouldBeTheSumOfPrices";
-        final FileCalculator fileCalculator = buildFileCalculatorForTestWithOrderPrice(testName, TASK);
-        final File expected = withTestName.apply(testName, getResult).apply(TASK);
+        final OrderPriceCalculator orderPriceCalculator = buildCalculator(testName);
+        final File expected = getExpected(testName);
         //When
-        final File result = fileCalculator.calculateOrderPrices();
+        final File result = orderPriceCalculator.calculateOrderPrices();
         //Then
         assertThat(result).exists();
         assertThat(contentOf(result))
@@ -76,10 +72,10 @@ class OrderPriceCalculatorShould {
     void whenThereIsMultipleOrdersYouGetTheSumPriceOfItsProductsForEachOrder() throws IOException {
         //Given
         final String testName = "whenThereIsMultipleOrdersYouGetTheSumPriceOfItsProductsForEachOrder";
-        final FileCalculator fileCalculator = buildFileCalculatorForTestWithOrderPrice(testName, TASK);
-        final File expected = withTestName.apply(testName, getResult).apply(TASK);
+        final OrderPriceCalculator orderPriceCalculator = buildCalculator(testName);
+        final File expected = getExpected(testName);
         //When
-        final File result = fileCalculator.calculateOrderPrices();
+        final File result = orderPriceCalculator.calculateOrderPrices();
         //Then
         assertThat(result).exists();
         assertThat(contentOf(result))
@@ -88,51 +84,12 @@ class OrderPriceCalculatorShould {
 
     }
 
-
-    //Helper methods and closures to reduce boilerplate to get the files
-    private File getResourceFile(final String task, final String testName, final String fileName) {
-
-        final String pathToFile = "/" + task + "/" + testName + "/" + fileName;
-
-        return new File(getClass().getResource(pathToFile).getFile());
-    }
-
+    //Utils
     private File getResourceFile(final String testName, final String fileName) {
 
         final String pathToFile = "/" + TASK + "/" + testName + "/" + fileName;
 
         return new File(getClass().getResource(pathToFile).getFile());
-    }
-
-    private BiFunction<String, String, File> getProducts =
-            (String task, String url) -> getResourceFile(task, url, PRODUCTS_CSV);
-    private BiFunction<String, String, File> getOrders =
-            (String task, String url) -> getResourceFile(task, url, ORDERS_CSV);
-    private BiFunction<String, String, File> getCustomers =
-            (String task, String url) -> getResourceFile(task, url, CUSTOMERS_CSV);
-    private BiFunction<String, String, File> getResult =
-            (String task, String url) -> getResourceFile(task, url, RESULT_CSV);
-
-    private BiFunction<String, BiFunction<String, String, File>, Function<String, File>> withTestName
-            = (String testName, BiFunction<String, String, File> onGetter) ->
-            taskName -> onGetter.apply(taskName, testName);
-
-    private FileCalculator buildFileCalculatorForTest(String testName, String taskName) {
-        final File customers = withTestName.apply(testName, getCustomers).apply(taskName);
-        final File products = withTestName.apply(testName, getProducts).apply(taskName);
-        final File orders = withTestName.apply(testName, getOrders).apply(taskName);
-
-        return new FileCalculator(customers, products, orders, OUT_DIRECTORY);
-    }
-
-    private FileCalculator buildFileCalculatorForTestWithOrderPrice(String testName, String taskName) {
-        final File customers = withTestName.apply(testName, getCustomers).apply(taskName);
-        final File products = withTestName.apply(testName, getProducts).apply(taskName);
-        final File orders = withTestName.apply(testName, getOrders).apply(taskName);
-
-        final OrderPriceCalculator orderPriceCalculator = new OrderPriceCalculator(products, orders, OUT_DIRECTORY);
-
-        return new FileCalculator(customers, products, orders, OUT_DIRECTORY, orderPriceCalculator);
     }
 
     private OrderPriceCalculator buildCalculator(final String testName) {
